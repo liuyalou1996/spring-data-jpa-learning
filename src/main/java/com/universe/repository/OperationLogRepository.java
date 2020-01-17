44d1e5c04e71418b5e8712c56e3a80eac76019c8
liuyalou <liuyalou@iboxpay.com>
2020-01-17 10:53:26 +0800

    添加示例

--- src/main/java/com/universe/repository/OperationLogRepository.java
+++ src/main/java/com/universe/repository/OperationLogRepository.java
@@ -1,9 +1,14 @@
 package com.universe.repository;
 
-import org.springframework.data.jpa.repository.JpaRepository;
-
 import com.universe.entity.OperationLog;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 public interface OperationLogRepository extends JpaRepository<OperationLog, Long> {
 
+	@Query("select t from OperationLog t")
+	List<OperationLog> findAllOperationLog(Pageable pageable);
 }
--- src/test/java/com/universe/test/OperationLogTest.java
+++ src/test/java/com/universe/test/OperationLogTest.java
@@ -1,36 +1,43 @@
 package com.universe.test;
 
-import java.util.Date;
-
+import com.universe.Application;
+import com.universe.entity.OperationLog;
+import com.universe.repository.OperationLogRepository;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.test.context.junit4.SpringRunner;
 
-import com.universe.Application;
-import com.universe.entity.OperationLog;
-import com.universe.repository.OperationLogRepository;
+import java.util.Date;
+import java.util.List;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest(classes = Application.class)
 public class OperationLogTest {
 
-  @Autowired
-  private OperationLogRepository repository;
+	@Autowired
+	private OperationLogRepository repository;
 
-  @Test
-  public void insertRecord() {
-    OperationLog log = new OperationLog();
-    log.setId(1);
-    log.setOpType(1);
-    log.setOpContent("修改用户");
-    log.setUserId(1);
-    log.setCreateTime(new Date());
-    repository.save(log);
-    // save执行时会自动为持久化对象设置主键值
-    System.err.println(log.getId());
+	@Test
+	public void queryOperationLog() {
+		List<OperationLog> list = repository.findAllOperationLog(PageRequest.of(0, 1));
+		System.out.println(list);
+	}
 
-  }
+	@Test
+	public void insertRecord() {
+		OperationLog log = new OperationLog();
+		log.setId(1);
+		log.setOpType(1);
+		log.setOpContent("修改用户");
+		log.setUserId(1);
+		log.setCreateTime(new Date());
+		repository.save(log);
+		// save执行时会自动为持久化对象设置主键值
+		System.err.println(log.getId());
+
+	}
 
 }
